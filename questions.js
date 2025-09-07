b64445880a86cea3663779360f8529f101a8e958
Jlv-19 <vsree6751@gmail.com>
2025-09-07 14:24:25 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,27 +1,32 @@
 const questions = [
   {
-    question: "What is the capital of France?",
-    options: ["Berlin", "Madrid", "Paris", "Lisbon"],
-    answer: 2 // Paris
-  },
-  {
-    question: "Which language is used for web development?",
-    options: ["Python", "HTML", "C++", "Java"],
-    answer: 1 // HTML
-  },
-  {
-    question: "Which is the largest planet in our Solar System?",
-    options: ["Earth", "Saturn", "Jupiter", "Mars"],
-    answer: 2 // Jupiter
-  },
-  {
-    question: "What does CSS stand for?",
+    question: "What does HTML stand for?",
     options: [
-      "Cascading Style Sheets",
-      "Computer Style Sheets",
-      "Creative Style System",
-      "Colorful Style Syntax"
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyper Tool Multi Language"
     ],
-    answer: 0 // Cascading Style Sheets
+    answer: 2
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    options: [
+      "HTML",
+      "JQuery",
+      "CSS",
+      "XML"
+    ],
+    answer: 2
+  },
+  {
+    question: "What does JS stand for?",
+    options: [
+      "Java Script",
+      "Java Source",
+      "Just Script",
+      "JSON Script"
+    ],
+    answer: 0
   }
 ];
